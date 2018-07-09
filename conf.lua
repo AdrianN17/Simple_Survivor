357fa5063a431ad62f04951d16c05561e0c8b0f0
Adrian <36133718+AdrianN17@users.noreply.github.com>
2018-07-08 23:25:44 -0500

    Autor

--- conf.lua
+++ conf.lua
@@ -3,6 +3,6 @@ function love.conf(t)
   t.title="Simple Survivor" 
   t.window.width = 800
   t.window.height = 800
-  t.author="AdrianN"
+  t.author="AdrianN17"
   t.window.icon = "assets/icon.png"
 end
